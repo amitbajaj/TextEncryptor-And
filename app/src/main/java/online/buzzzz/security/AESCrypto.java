539bd99771c0cd7f199ff60005bf3887d9432dde
amit@dell <amit@dell.localhost>
2017-05-02 20:35:17 +0300

    Align with other platforms

--- app/src/main/java/online/buzzzz/security/AESCrypto.java
+++ app/src/main/java/online/buzzzz/security/AESCrypto.java
@@ -2,6 +2,7 @@ package online.buzzzz.security;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import javax.crypto.BadPaddingException;
@@ -11,42 +12,35 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import android.util.Base64;
+import org.apache.commons.codec.binary.Hex;
 
 public class AESCrypto {
 
-	static final int KEYLENGTH = 16;
-        static final int IVLENGTH = 16;
-        static final String ALGORITHM = "AES/CBC/PKCS5PADDING";
+    private static final int KEYLENGTH = 16;
+    private static final int IVLENGTH = 16;
+    private static final String ALGORITHM = "AES/CBC/PKCS5PADDING";
 	
-        public static String MASTERIV;
-        public static String MASTERKey;
-        
 	public static String encrypt(String key, String value){
 		try{
-			SecureRandom random = new SecureRandom();
-			byte[] randBytes = new byte[IVLENGTH];
-			random.nextBytes(randBytes);
-			IvParameterSpec iv = new IvParameterSpec(randBytes);
-                        String key2use=key;
-			
-			if(key.length()< KEYLENGTH){
-                            key2use = String.format("%0"+(KEYLENGTH-key.length())+"d%s", 0, key);
-			}else if(key.length()>KEYLENGTH){
-                            key2use = key.substring(0,KEYLENGTH);
-                        }
-			SecretKeySpec skeySpec;
-                        skeySpec = new SecretKeySpec(key2use.getBytes("UTF-8") , "AES");
-			
-			Cipher cipher = Cipher.getInstance(ALGORITHM);
-			cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
-			
-			byte[] staged = cipher.doFinal(value.getBytes());
-			byte[] encrypted = new byte[staged.length+randBytes.length];
-			System.arraycopy(randBytes, 0, encrypted, 0, randBytes.length);
-			System.arraycopy(staged, 0, encrypted, randBytes.length, staged.length);
-                        MASTERIV = new String(randBytes);
-                        MASTERKey = key2use;
-			return Base64.encodeToString(encrypted,Base64.DEFAULT);
+                    SecureRandom random = new SecureRandom();
+                    byte[] randBytes = new byte[IVLENGTH];
+                    MessageDigest md = MessageDigest.getInstance("SHA-256"); 
+                    random.nextBytes(randBytes);
+                    IvParameterSpec iv = new IvParameterSpec(randBytes);
+
+                    String key2use=Hex.encodeHexString(md.digest(key.getBytes("UTF-8"))).toLowerCase().substring(0, KEYLENGTH);
+                    SecretKeySpec skeySpec;
+                    System.out.println(key2use.getBytes().length);
+                    skeySpec = new SecretKeySpec(key2use.getBytes("UTF-8") , "AES");
+
+                    Cipher cipher = Cipher.getInstance(ALGORITHM);
+                    cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+                    byte[] staged = cipher.doFinal(value.getBytes());
+                    byte[] encrypted = new byte[staged.length+randBytes.length];
+                    System.arraycopy(randBytes, 0, encrypted, 0, randBytes.length);
+                    System.arraycopy(staged, 0, encrypted, randBytes.length, staged.length);
+                    return Base64.encodeToString(encrypted,Base64.DEFAULT);
 		} catch (UnsupportedEncodingException | InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException ex) {
 		    return ex.toString();
 		}
@@ -56,14 +50,9 @@ public class AESCrypto {
             byte[] ivBytes = new byte[IVLENGTH];
             byte[] staged = Base64.decode(value,Base64.DEFAULT);
             byte[] encrypted = new byte[staged.length-IVLENGTH];
-            String key2use=key;
-
-            if(key.length()< KEYLENGTH){
-                key2use = String.format("%0"+(KEYLENGTH-key.length())+"d%s", 0, key);
-            }else if(key.length()>KEYLENGTH){
-                key2use = key.substring(0,KEYLENGTH);
-            }
-            
+            MessageDigest md = MessageDigest.getInstance("SHA-256"); 
+            String key2use=Hex.encodeHexString(md.digest(key.getBytes("UTF-8"))).toLowerCase().substring(0,KEYLENGTH);
+            System.out.println(key2use.getBytes().length);
             System.arraycopy(staged, 0, ivBytes, 0, ivBytes.length);
             System.arraycopy(staged, ivBytes.length, encrypted, 0, staged.length-ivBytes.length);
             IvParameterSpec iv = new IvParameterSpec(ivBytes);
@@ -73,8 +62,6 @@ public class AESCrypto {
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
 
             byte[] original = cipher.doFinal(encrypted);
-            MASTERIV = new String(ivBytes);
-            MASTERKey = key2use;
 
             return new String(original);
         } catch (UnsupportedEncodingException | InvalidAlgorithmParameterException | InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException ex) {
